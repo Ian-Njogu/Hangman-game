24888f79f28ab3db4a6c1fd75916b3350789242a
Ian Njogu <robustian16@gmail.com>
2025-06-11 18:21:29 +0300

    Validating user input

--- hangman.py
+++ hangman.py
@@ -5,4 +5,19 @@ def hangman_game():
     word = random.choice(words)
     word_display = ['_'] * len(word)
     attempts = 6
-    guessed_letters = set()
+    guessed_letters = set() #FOR INPUT VALIDATION 
+
+    print("Welcome to the hangman game!!!\n Guess the words in less than 6 attempts")
+    print("".join(word_display))
+
+    while attempts >0 and '_' in word_display:
+        guess = input("Enter a letter: ").lower()
+
+        try:
+            if len(guess) !=1 or not guess.alpha():
+                raise ValueError("Please enter a single alphabetical letter")
+            if guess in guessed_letters:
+                raise ValueError("You've already entered that letter")
+        except ValueError as ve:
+            print(ve)
+
